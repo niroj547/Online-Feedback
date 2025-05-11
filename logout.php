edb7448b9a749224c1b914ba83a5cc7cf64fb68f
ugeshkc <np03cs4s240088@heraldcollege.edu.np>
2025-05-11 17:02:43 +0545

    codes

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();              // Start the session
-session_unset();              // Clear all session variables
-session_destroy();            // Destroy the session completely
-
-// Redirect to admin login page
-header("Location: admin_login.html");
-exit();
-?>
